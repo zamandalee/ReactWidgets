d711592f4f7ef768b8e964ec8ca69e90c83b8502
Amanda <a@zalee.me>
2018-09-27 13:30:58 -0700

    Finish clock widget

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { time: new Date() };
+
+    this.tick = this.tick.bind(this);
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval( this.tick, 1000 );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  tick() {
+    this.setState({time: new Date()});
+  }
+
   render() {
+    let hours = this.state.time.getHours();
+    let minutes = this.state.time.getMinutes();
+    let seconds = this.state.time.getSeconds();
+
+    hours = (hours < 10) ? `0${hours}` : hours;
+    minutes = (minutes < 10) ? `0${minutes}` : minutes;
+    seconds = (seconds < 10) ? `0${seconds}` : seconds;
+    // console.log(`0${hours}`);
+
     return (
-      <h1>Clock</h1>
+      <div className="clock-widget">
+        <h1>Clock</h1>
+        <div className="clock-content">
+          <div>
+            <h2>Time:</h2>
+            <h2>{hours} : {minutes} : {seconds} PDT</h2>
+          </div>
+          <div>
+            <h2>Date:</h2>
+            <h2>{ this.state.time.toDateString() }</h2>
+          </div>
+        </div>
+      </div>
+
     );
   }
 }
